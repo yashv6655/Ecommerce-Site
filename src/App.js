af23434129ee414cfe9e336c46e85a6bc2235a14
Yash Velagapudi <yashv6655@gmail.com>
2020-04-19 21:15:31 -0700

    Updated for functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import styled from "styled-components";
+import {Route, Switch} from "react-router-dom";
 
-function App() {
+import AboutPage from "./pages/AboutPage";
+import CartPage from "./pages/CartPage";
+import ContactPage from "./pages/ContactPage";
+import DefaultPage from "./pages/DefaultPage";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import SPPage from "./pages/SingleProductPage";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import SideCart from "./components/SideCart";
+import Footer from "./components/Footer";
+
+class App extends Component{
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Navbar, Sidebar, Cart, Footer */}
+      <Navbar />
+      <Sidebar />
+      <SideCart />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/products" exact component={ProductsPage} />
+        <Route path="/products/:id" component={SPPage} />
+        <Route path="/cart" component={CartPage} />
+        <Route component={DefaultPage}/>
+      </Switch>
+      <Footer />
+    </>
   );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from "react-router-dom";
+import {ProductProvider} from "./context";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>,
   document.getElementById('root')
 );
 
