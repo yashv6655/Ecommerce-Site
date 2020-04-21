5b650cd545c91c8dbb8214eaab12deda0e87c986
Yash Velagapudi <yashv6655@gmail.com>
2020-04-20 20:21:32 -0700

    Added .env File

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { linkData } from "./linkData";
 import { socialData } from "./socialData";
-import { items } from "./productData";
+// import { items } from "./productData";
+import { client } from "./Contentful";
+
 const ProductContext = React.createContext();
 //Provider
 //Consumer
@@ -30,8 +32,13 @@ class ProductProvider extends Component {
   };
   componentDidMount() {
     //from contentful items
-
-    this.setProducts(items);
+    //this.setProducts(items);
+    client
+      .getEntries({
+        content_type: "techStoreProducts",
+      })
+      .then((response) => this.setProducts(response.items))
+      .catch(console.error);
   }
 
   //set products
